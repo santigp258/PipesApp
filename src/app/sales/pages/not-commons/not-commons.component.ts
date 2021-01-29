c737538a03543d5f6c7e5534dc489455cbb2d347
Santiago <santigp258@gmail.com>
2021-01-29 18:02:30 -0500

    i18nPlural pipe

--- src/app/sales/pages/not-commons/not-commons.component.ts
+++ src/app/sales/pages/not-commons/not-commons.component.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NotCommonsComponent {
+  //i18nSelect
   name: string = 'Santi';
   gender: string = 'male';
   inviteMap: any = { female: 'invitarla', male: 'invitarlo' };
+
+  //i18nPlural
+
+  clients: string[] = ['Ana', 'Santi'];
+  clientsMapping = {
+    '=0': 'no tenemos ningún cliente esperando',
+    'other' : 'tenemos # clientes esperando'
+  }
 }
