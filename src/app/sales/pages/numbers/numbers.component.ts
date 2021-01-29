4b569a78ec65eb582003ff6ccbc08d15b1dbf043
Santiago <santigp258@gmail.com>
2021-01-29 17:09:02 -0500

    decimal pipe

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumbersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NumbersComponent {
+  totalSales: number = 204040404.262822;
+  percentage: number = 0.48;
 
 }
